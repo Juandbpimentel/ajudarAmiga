7a5b10901dbd1f02adfac256575e384021891358
Juan Pimentel <juandbpimentel@alu.ufc.br>
2022-06-25 22:08:38 -0300

    Correções no modelo do livro

--- backend_yara/models/book.model.js
+++ backend_yara/models/book.model.js
@@ -1,15 +1,18 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
 
 var BookSchema = new Schema({
 	name: { type: String, required: true, max: 200 },
 	description: { type: String, required: true, max: 1000 },
 	price: { type: Number, required: true },
 	genre: { type: String, required: true, max: 200 },
+	author: { type: String, required: true, max: 200 },
+	edition: { type: String, required: true, max: 200 },
+	quantity: { type: Number, required: true },
 	image: {
 		type: String,
 		required: false,
 	},
-});
+})
 
-module.exports = Book = mongoose.model('Books', BookSchema);
+module.exports = Book = mongoose.model('Books', BookSchema)
